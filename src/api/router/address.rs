219db8921deeeb70a4cada203774b725d16a93da
karneges <karneges2012@gmail.com>
2023-09-13 16:22:45 +0400

    fix

--- src/api/router/address.rs
+++ src/api/router/address.rs
@@ -8,6 +8,7 @@ use crate::api::controllers;
 pub fn router() -> Router {
     Router::new()
         .route("/check", post(controllers::post_address_check))
+        .route("/add", post(controllers::post_add_account_subscription))
         .route("/create", post(controllers::post_address_create))
         .route("/:address", get(controllers::get_address_balance))
         .route("/:address/info", get(controllers::get_address_info))
